37c6fdfbec4520644a50c995ff8fbb4741d97ad7
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-08 01:29:35 +0530

    script to create vite with tailwind project

---
+++ viteXtailwindcss.py
@@ -0,0 +1,34 @@
+import os
+
+app_name = "my-app"
+
+while True:
+    app_name = input("\n? Project/App name : ")
+    if not input("Do you want to Edit?(y = Yes, Enter = No):").lower() == "y":
+        break
+
+os.system(f"npm create vite@latest {app_name}")
+os.system(f"cd {app_name} && npm install -D tailwindcss@latest postcss@latest autoprefixer@latest")
+os.system(f"cd {app_name} && npx tailwindcss init -p")
+
+try:
+    file_name = f"./{app_name}/tailwind.config.js"
+    file_contents = None
+    with open(file_name, 'r') as file:
+        file_contents = file.read()
+    file_contents = file_contents.replace(r"content: []", r'content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}", ]')
+    try:
+        with open(file_name, 'w') as file:
+            file.write(file_contents)
+    except Exception as e:
+        exit("Error:", str(e))
+except:
+    exit("Somthing went wrong!")
+
+try:
+    with open(f"./{app_name}/src/index.css", 'w') as file:
+        file.write("\n@tailwind base;\n@tailwind components;\n@tailwind utilities;\n")
+except Exception as e:
+    exit("Error:", str(e))
+
+os.system(f"cd {app_name} && npm run dev")
