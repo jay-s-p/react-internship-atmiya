12f6b4718b3353a539acefbe9878ec8c0dade510
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-10 10:01:47 +0530

    updated config file

--- pizza-order-app/vite.config.js
+++ pizza-order-app/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: "192.168.1.244"
-    host: "192.168.1.243"
+    host: ["192.168.1.244", "192.168.1.243"],
+    port: 80,
   },
 })
