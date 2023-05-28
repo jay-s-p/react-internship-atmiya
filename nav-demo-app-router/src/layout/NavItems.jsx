80709a8a32fa6b940547cddbe1ceeac46bb09a59
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-29 02:17:31 +0530

    Updated navbar with responsive design

--- nav-demo-app-router/src/layout/NavItems.jsx
+++ nav-demo-app-router/src/layout/NavItems.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
 
-const NavItems = ({ navText, navPath, name, className }) => {
+const NavItems = ({ navText, navPath, name, className, setNavOpen }) => {
   return (
     <>
-      <Link to={navPath} >
+      <Link to={navPath} onClick={() => setNavOpen(x => !x)} >
         <label className={`${className} text-gray-800 cursor-pointer  px-3 py-2 rounded-md hover:bg-gray-300 active:bg-gray-400`}>
           <input type="radio" name={name} className="hidden" />
           <span className="capitalize">
--- nav-demo-app-router/src/layout/Navbar.jsx
+++ nav-demo-app-router/src/layout/Navbar.jsx
@@ -1,10 +1,17 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import NavItems from "./NavItems"
+import React, { useEffect, useState } from 'react';
 
 
 const Navbar = ({ navItems }) => {
 
   const location = useLocation();
+
+  const [navOpen, setNavOpen] = useState(false);
+
+  useEffect(() => {
+    setNavOpen(false);
+  }, [location]);
 
   const isActive = (pathname) => {
     return location.pathname === pathname ? 'bg-gray-300' : '';
@@ -16,23 +23,31 @@ const Navbar = ({ navItems }) => {
         <div className="mx-auto h-fit">
           <div className="h-fit flex flex-col items-center justify-between 
                           md:flex-row ">
-            <input className='peer hidden navbar-main' type="checkbox" id="navbar-inp" />
+            <input className=' hidden navbar-main' type="checkbox" id="navbar-inp" />
             {/* Title text */}
             <div className="h-[5vh]  w-full flex justify-between items-center my-5 px-6 flex-shrink-0
                             md:w-fit">
               <a href="/" className="text-gray-600 font-bold text-4xl font-serif select-none transition-all duration-500 hover:text-white ">Z z z</a>
               <label htmlFor="navbar-inp" className=' block md:hidden'>
-                <svg className='' viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="M21.75 18.75H2.25a.75.75 0 0 1 0-1.5h19.5a.75.75 0 0 1 0 1.5zm0-6H2.25a.75.75 0 0 1 0-1.5h19.5a.75.75 0 0 1 0 1.5zm0-6H2.25a.75.75 0 0 1 0-1.5h19.5a.75.75 0 0 1 0 1.5z"></path></svg>
+                {/* <svg className='' viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="M21.75 18.75H2.25a.75.75 0 0 1 0-1.5h19.5a.75.75 0 0 1 0 1.5zm0-6H2.25a.75.75 0 0 1 0-1.5h19.5a.75.75 0 0 1 0 1.5zm0-6H2.25a.75.75 0 0 1 0-1.5h19.5a.75.75 0 0 1 0 1.5z"></path></svg> */}
+                <svg className='h-6 w-6' stroke="currentColor" fill="none" viewBox="0 0 24 24" onClick={() => setNavOpen(!navOpen)}>
+                  <path className={`${navOpen ? "hidden" : "block"} `} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                  <path className={`${navOpen ? "block" : "hidden"} `} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
               </label>
             </div>
 
             {/* Navigation Links */}
-            <div className="hidden peer-checked:block w-full h-[95vh] ring-0 ring-inset ring-red-500 bg-gray-200  mx-5
-                             md:block md:h-fit md:bg-transparent ">
+            <div className={`${navOpen ? "block" : "hidden"}  w-full h-[95vh] ring-0 ring-inset ring-red-500 bg-gray-200  mx-5
+                             md:block md:h-fit md:bg-transparent `}>
               <div className="h-full flex flex-col justify-center items-center gap-16 font-bold text-3xl
              md:text-xl md:flex-row md:justify-end">
                 {
-                  navItems.map((nav, id) => <NavItems className={isActive(nav[1])} navText={nav[0]} navPath={nav[1]} name="nav-bar" />)
+                  navItems.map((nav, id) =>
+                    <React.Fragment key={id}>
+                      <NavItems setNavOpen={setNavOpen} className={isActive(nav[1])} navText={nav[0]} navPath={nav[1]} name="nav-bar" />
+                    </React.Fragment>
+                  )
                 }
               </div>
             </div>
