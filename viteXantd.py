08b93b3a1688469b512fb23582755e4ed9503b85
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-28 14:55:55 +0530

    update vite project scripts

--- viteXantd.py
+++ viteXantd.py
@@ -8,28 +8,29 @@ while True:
         break
 
 os.system(f"npm create vite@latest {app_name}")
-os.system(f"cd {app_name} && npm install -D tailwindcss@latest postcss@latest autoprefixer@latest")
-os.system(f"cd {app_name} && npx tailwindcss init -p")
+os.system(f"cd {app_name} && npm i antd")
 
-try:
-    file_name = f"./{app_name}/tailwind.config.js"
-    file_contents = None
-    with open(file_name, 'r') as file:
-        file_contents = file.read()
-    file_contents = file_contents.replace(r"content: []", r'content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}", ]')
-    try:
-        with open(file_name, 'w') as file:
-            file.write(file_contents)
-    except Exception as e:
-        exit("Error:", str(e))
-except:
-    exit("Somthing went wrong!")
 
 try:
     with open(f"./{app_name}/src/index.css", 'w') as file:
-        file.write("\n@tailwind base;\n@tailwind components;\n@tailwind utilities;\n")
+        file.write('''
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'
+}
+
+html,
+body {
+  height: 100vh;
+  width: 100vw;
+}
+        ''')
 except Exception as e:
     exit("Error:", str(e))
+
 
 try:
     file_name = f"./{app_name}/vite.config.js"
@@ -38,10 +39,10 @@ try:
         file_contents = file.read()
     file_contents = file_contents.replace(
         r"})",
-        r'''  server: {
-    host: ["192.168.1.244", "192.168.1.243", "localhost"],
-    port: 80,
-  },
+        r'''//  server: {
+  //  host: ["192.168.1.244", "192.168.1.243", "localhost"],
+  //  port: 80,
+  // },
 })''')
     try:
         with open(file_name, 'w') as file:
