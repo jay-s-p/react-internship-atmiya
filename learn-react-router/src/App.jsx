e54fa9d3c5a4d18a6a17b977325ce4a82789fb80
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-08-25 12:16:34 +0530

    Implement Router code - add 2 new page : home, about - main.jsx : Define Router - App.jsx : Define home and about routes and nav links - Update the style

--- learn-react-router/src/App.jsx
+++ learn-react-router/src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <nav >
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   )
 }
