2728cae7474177ed28cffcf55567c2c2c3c024e0
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-08-25 13:00:36 +0530

    Advance in NavLink

--- learn-react-router/src/App.jsx
+++ learn-react-router/src/App.jsx
@@ -4,11 +4,21 @@ import Home from './pages/Home'
 import About from './pages/About'
 
 function App() {
+
+  const navClassName = ({ isActive }) =>
+    isActive ?
+      "nav-active" // This class will be added to the active link.
+      :
+      "" // This class will be added to the inactive link.
+
   return (
     <>
-      <nav >
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
+      <nav>
+        {/* By default, NavLink will add the class "active" to the active link. */}
+        {/* We can override this behavior by passing a function to the "className" prop. */}
+        {/* The function will receive an object with a property "isActive" that will be true if the link is active. */}
+        <NavLink className={navClassName} to="/">Home</NavLink>
+        <NavLink className={navClassName} to="about">About</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
